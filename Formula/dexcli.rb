47432a8ae1e18eba582f4cc5d53dead3d0daaedd
Quanzheng Long <qlong.seattle@gmail.com>
2026-09-09 19:42:07 -0700

    dexcli 0.3.1

--- Formula/dexcli.rb
+++ Formula/dexcli.rb
@@ -1,28 +1,28 @@
 class Dexcli < Formula
   desc "Develop and operate Dex from the command line"
   homepage "https://github.com/superdurable/dex"
-  version "0.1.21"
+  version "0.3.1"
   license "MIT"
 
   depends_on "temporal"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/superdurable/dex/releases/download/cli-v0.1.21/dexcli_v0.1.21_darwin_arm64.tar.gz"
-      sha256 "8d5450184dbb7b10ff699a045bac0478e95214dad0caf1fdaa51cdf5af7d79c1"
+      url "https://github.com/superdurable/dex/releases/download/cli-0.3.1/dexcli_0.3.1_darwin_arm64.tar.gz"
+      sha256 "d07aada6e88f9ac5a3b83013edfb5f2b571c529e8d9a38fc5cb5fa0ae0c18f4e"
     else
-      url "https://github.com/superdurable/dex/releases/download/cli-v0.1.21/dexcli_v0.1.21_darwin_amd64.tar.gz"
-      sha256 "c907c30862d00e8fc1df73f4eb4523a9e65ff6a4cef6feb8756ee5c89405a4a1"
+      url "https://github.com/superdurable/dex/releases/download/cli-0.3.1/dexcli_0.3.1_darwin_amd64.tar.gz"
+      sha256 "2d2081ac9c6922f2d24f93b9f63adb44e9507ee37379cda960b1e54c695a36b8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/superdurable/dex/releases/download/cli-v0.1.21/dexcli_v0.1.21_linux_arm64.tar.gz"
-      sha256 "4ecafb61bdead256c69fcebc73890706aea95d0d257a4eaceb2de4f62968a637"
+      url "https://github.com/superdurable/dex/releases/download/cli-0.3.1/dexcli_0.3.1_linux_arm64.tar.gz"
+      sha256 "2dfbf1e744910f4b01997f7719d8d0b729cc9a2cdb3848db64573d72cdcf3d02"
     else
-      url "https://github.com/superdurable/dex/releases/download/cli-v0.1.21/dexcli_v0.1.21_linux_amd64.tar.gz"
-      sha256 "1a2b554fc4ee459ad63dcd5806875022cb7fec193f62fb17ccb657306995a77f"
+      url "https://github.com/superdurable/dex/releases/download/cli-0.3.1/dexcli_0.3.1_linux_amd64.tar.gz"
+      sha256 "d4918ca75c65e89594d45be5d08615f53b1e612d7827a9a14b9046b9e911c0f1"
     end
   end
 
